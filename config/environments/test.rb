f442c65b14425406971f80d6b4c97a8d180be138
Josh Smith <josh@jshsmth.com>
2022-05-28 19:39:22 +1000

    deployment route issue debugging

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
+    # cloudinary
+    config.active_storage.service = :cloudinary
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
