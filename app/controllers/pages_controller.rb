8985f7378743cb6579dd7186e91f1827decfc518
Josh Smith <josh@jshsmth.com>
2022-06-16 01:20:04 -0700

    Search homepage function (#15)
    * chore: start search function
    
    * chore: start search function
    
    * chore: start search function
    
    * chore: start search function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @popular = RSpotify::Artist.search('Vance')
+    @popular = RSpotify::Artist.search('Greenday')
     @friend = RSpotify::Album.search('Cat')
     @trending = RSpotify::Recommendations.generate(seed_genres: ['rock', 'pop'])
+  end
+
+  def show_search_results
+    @search_text = RSpotify::Artist.search(params[:searchText]).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#home'
+  get 'search', to: 'pages#show_search_results'
+  devise_for :users
 end
