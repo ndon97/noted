7ac8d1caad758f734a939d8efd230a5da78f06dd
Josh Smith <josh@jshsmth.com>
2022-06-18 20:37:44 -0700

    template segments for homepage (#20)
    * begin template
    
    * begin template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @popular = RSpotify::Artist.search('Greenday')
-    @friend = RSpotify::Album.search('Cat')
-    @trending = RSpotify::Recommendations.generate(seed_genres: ['rock', 'pop'])
   end
 
   def show_search_results
