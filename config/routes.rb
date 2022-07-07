d8d76c010525b0b82f7bfbd146803522dac79f1e
Josh Smith <josh@jshsmth.com>
2022-07-07 02:02:18 -0700

    Add/routes controllers to noted (#37)
    * routes done
    
    * controllers completed
    
    * added services folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'search', to: 'pages#show_search_results'
   devise_for :users
+
+  resources :artists, only: [:index, :show]
+  resources :songs do
+    resources :song_reviews, only: [:create, :index, :show, :edit, :update, :new]
+  end
+  resources :song_reviews, only: [:destroy]
+  resources :albums do
+    resources :albums_reviews, only: [:create, :index, :show, :edit, :update, :new]
+  end
+  resources :album_reviews, only: [:destroy]
 end
