1c5354036c763adb196623a2b798a08d488e3f15
Nick Don <98744275+ndon97@users.noreply.github.com>
2022-07-16 10:57:12 +1000

    Album review controller (#58)
    * album review logic
    
    * controller logic and reviews
    
    * remove raise
    
    Co-authored-by: Josh Smith <josh@jshsmth.com>

--- app/controllers/album_reviews_controller.rb
+++ app/controllers/album_reviews_controller.rb
@@ -1,8 +1,24 @@
 class AlbumReviewsController < ApplicationController
   def show
-      @album = MusicAPI.new.findAlbum('7mgdTKTCdfnLoa1HXHvLYM')
+      @album = MusicAPI.new.findAlbum(params[:id])
       artistid = @album.artists[0].id
       @artist = MusicAPI.new.findArtist(artistid)
       @album_review = AlbumReview.new
+  end
+
+  def create
+    @album_review = AlbumReview.new(review_params)
+    @album = MusicAPI.new.findAlbum(params[:id])
+    @album_review.api_id = @album.id
+    @album_review.user_id = current_user.id
+    if @album_review.save
+      redirect_to show
+    else
+      render :show
+    end
+  end
+
+  def review_params
+    params.require(:album_review).permit(:description, :rating)
   end
 end
