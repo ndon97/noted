82a0630fa890278667c052d4504fd722892f78da
Panashe Mugayapi <pmugayapi@hotmail.com>
2022-07-19 21:13:47 +1000

    recent review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @album_reviews = AlbumReview.where(user_id: @user.id)
     @count = AlbumReview.where(user_id: @user.id)
+
+    @songs_reviews = SongReview.where(user_id: @user.id)
   end
 end
