6ce240819ca1717a42b727a8faddcd062fda8f65
z-pl <60358608+z-pl@users.noreply.github.com>
2022-07-23 11:06:29 +1000

    Artists review page (#79)
    * most reviewed albums section
    
    * show me button
    
    * style changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
 
   def home
-    @artists = MusicAPI.new.displayArtist('frank')
-    @albums = MusicAPI.new.displayAlbums('playboi')
+    @artists = MusicAPI.new.displayArtist('ariana')
+    @albums = MusicAPI.new.displayAlbums('lil')
     @tracks = MusicAPI.new.displayTracks('bush')
   end
 
@@ -10,12 +10,14 @@ class PagesController < ApplicationController
     # if general search term is provided, search for artist, album, and track
     if params[:searchText]
       @resultsArtist = MusicAPI.new.findArtist(params[:searchText])
-      @top_tracks = @resultsArtist.top_tracks(:US).first(4)
+
+      @top_tracks = @resultsArtist.top_tracks(:US)
+
       @top_albums = @resultsArtist.albums(limit: 8, country: 'US')
 
     else
       @resultsArtist = MusicAPI.new.findArtist(params[:id])
-      @top_tracks = @resultsArtist.top_tracks(:US).first(4)
+      @top_tracks = @resultsArtist.top_tracks(:US)
       @top_albums = @resultsArtist.albums(limit: 8, country: 'US')
     end
   end
