e603f5226ff16aad2196af91d836d906e55aad95
Panashe Mugayapi <pmugayapi@hotmail.com>
2022-07-23 11:11:26 +1000

    animations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @album_reviews = AlbumReview.where(user_id: @user.id)
-    @count = AlbumReview.where(user_id: @user.id)
     @song_reviews = SongReview.where(user_id: @user.id)
   end
 end
